d907cc278e2b94d8f2ede49ee75df66f47101397
LGvanPareren <320373@student.mboutrecht.nl>
2022-02-15 16:42:54 +0100

    15-2-2022 Changes: Inlog & Register form is ready to be used. Who: Luiz Guilherme

---
+++ php/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Log In</title>
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<div class="loginbox">
+		<img src="./src/img/newsicon.png" alt="" class="avatar">
+		<h1 class="logh1">New Account</h1>
+		<form>
+			<p>Username</p>
+			<input type="username" name="" placeholder="Enter Username..">
+			<p>E-mail</p>
+			<input type="mail" name="" placeholder="Enter E-mail..">
+			<p>Birthday</p>
+			<input type="date" name="" id="">
+			<p>Password</p>
+			<input type="password" name="" placeholder="Enter password..">
+			<p>Confirm Password</p>
+			<input type="password" name="" placeholder="Confirm password..">
+
+			<input type="submit" name="" value="Make Account">
+			<a href="../php/inlog.php">Do you have an account? Login.</a>
+		</form>
+
+	</div>
+</body>
+</html>	
